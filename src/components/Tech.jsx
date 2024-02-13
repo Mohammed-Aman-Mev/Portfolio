6fbff8588ff34aeb2bfdd36d8a7679f4d499dfc9
Mohammed Aman Mev <mohammedaman0731@gmail.com>
2024-02-13 12:48:37 +0530

    animation added

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -16,7 +16,9 @@ const Tech = () => {
   return (
     <div className="pt-[50px] sm:py-[100px] bg-[#020617] w-full">
       <div>
-        <h1 className="text-center text-5xl text-pink-500 my-[40px] font-bold">Skills</h1>
+        <h1 className="text-center text-5xl text-pink-500 my-[40px] font-bold">
+          Skills
+        </h1>
       </div>
       <div className="w-full flex flex-wrap gap-7 items-center justify-center p-3 sm:p">
         {arr.map((i) => {
--- src/components/TechSkill.jsx
+++ src/components/TechSkill.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const TechSkill = ({ item }) => {
   return (
-    <div className="w-[190px] h-[190px] flex items-center justify-center border-2 border-fuchsia-500 rounded-full">
-      <img src={item} alt="" className="w-[100px]" />
+    <div className="w-[190px] hover:animate-bounce hover:border-none h-[190px] flex items-center justify-center border-2 border-fuchsia-500 rounded-full">
+      <img src={item} alt="" className="w-[100px] hover:w-36" />
     </div>
   );
 };
